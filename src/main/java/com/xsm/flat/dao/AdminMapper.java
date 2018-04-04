f5d68c52767f62e8b1cb27e1369a5fa33b1320c2
fikyair <shiming_xsm@163.com>
2018-04-04 17:10:42 +0800

    finish user login

---
+++ src/main/java/com/xsm/flat/dao/AdminMapper.java
@@ -0,0 +1,10 @@
+package com.xsm.flat.dao;
+
+import com.xsm.flat.entity.Admin;
+
+import java.util.List;
+
+public interface AdminMapper {
+
+    Admin selectByPrimaryKey(Admin admin);
+}
---
+++ src/main/java/com/xsm/flat/entity/Admin.java
@@ -0,0 +1,48 @@
+package com.xsm.flat.entity;
+
+public class Admin {
+    private String username;
+
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        Admin other = (Admin) that;
+        return (this.getUsername() == null ? other.getUsername() == null : this.getUsername().equals(other.getUsername()))
+            && (this.getPassword() == null ? other.getPassword() == null : this.getPassword().equals(other.getPassword()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getUsername() == null) ? 0 : getUsername().hashCode());
+        result = prime * result + ((getPassword() == null) ? 0 : getPassword().hashCode());
+        return result;
+    }
+}
